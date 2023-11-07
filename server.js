34a7bc1e5bde60747883ae0d6d59f3d379add09d
pronoyc11 <pronoyc11@gmail.com>
2023-11-08 02:36:52 +0600

    last vai

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const DB = process.env.MONGODB_SERVER.replace("<PASSWORD>",process.env.DB_PASSWORD)
-
+global.__basedir = __dirname 
 mongoose.connect(DB)
         .then(res=>console.log("connected to MONGODB."))
         .catch(err => console.log(err))
